eb5f84d2a286f06279b01987009f6e592a613ee7
Magnus Harder <26723180+MJHC@users.noreply.github.com>
2025-04-26 17:08:35 +0200

    configs

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 curl -o init.sql https://raw.githubusercontent.com/SAM-Research/sam-instant-messenger/refs/heads/main/server/database/init.sql
+mv ./init.sql ./initdb/init.sql
 
-docker compose --env-file .env up
+docker compose up
---
+++ initdb/setup.sql
@@ -0,0 +1,4 @@
+INSERT INTO device_link_info
+    (id, link_secret, provision_expire_seconds)
+VALUES
+    (1, 'linktest', 600);
